c6fa726ab365bd88da384a07d4b84a34e52a04ec
sky6565 <godwin1988b@outlook.com>
2023-11-29 21:57:12 -0800

    Connection string

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 // const mongoose = require('mongoose');
 
-const {connect, connection} = require ('mongoose');
+const { connect, connection } = require("mongoose");
 
 // Wrap Mongoose around local connection to MongoDB
 // mongoose.connect('mongodb://127.0.0.1:27017/userDB');
-connect('mongodb://127.0.0.1:27017/userDB');
+connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/userDB");
 
 // Export connection
 // module.exports = mongoose.connection;
